3d1c41dbe66a339a02b9380157d04242e0053528
mctashuo <mctashuo@gmail.com>
2018-03-12 23:51:51 +0800

    各种返回值测试xml,html,json

---
+++ 07-returnjson.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+{title:'天龙八部',intro:'人生八苦'}
+*/
+$book = array('title'=>'天龙八部','intro'=>'人生八苦');
+echo json_encode($book);
+
+// json_decode
+
+?>
---
+++ 07-returnxml.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Content-Type: text/xml');
+?>
+<?xml version="1.0" encoding="utf-8"?>
+<bookstore><book bid="b008"><title>天龙八部</title><intro>人生太苦了</intro></book></bookstore>
